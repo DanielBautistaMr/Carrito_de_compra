08b274b5bceb482ada23ad24d3e357c61bf5255a
Dani <dani@192.168.1.17>
2023-09-03 18:36:52 -0500

    Se comenzo a construir las tarjetas de los productos por el momento solo 1

---
+++ index.php
@@ -0,0 +1,60 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Carrito de compra</title>
+ <!-- BOOSTRAP  CDN-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css" integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
+<!-- FONT ASWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+ 
+    <!-- ESTILOS -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+ <body>
+  
+<div class="container">
+    <div class="row text-center py-5"> 
+        <div class="col-md-3 col-sm-6 my-3 my-md-0">
+            <form action="index.php" method="post">
+                <div class="card shadow">
+                    <div>
+                        <img src="./img/avion.png" alt="img1" class="img-fluid card-img-top">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Avion</h5>
+                        <h6>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+
+
+                        </h6>
+                        <p class="card-text">
+                            Es el mejor avion para estrellarse
+                        </p>
+                        <h5 class="price">$911</h5>
+
+                        <button type="submit" name="agregar" class="btn btn-warning my-3">Agregar al Carrito <i class="fas fa-shopping-cart"></i></button>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-3 col-sm-6 my-3 my-md-0">
+            
+        </div>
+        <div class="col-md-3 col-sm-6 my-3 my-md-0">
+            
+        </div>
+    </div>
+</div>
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+</body>
+ </html>
