3749a472065ded143f57ca00a4fe4c055750b072
mmesa269 <mmesa269@unab.edu.co>
2023-09-03 21:19:07 -0500

    <Commit 6>

--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <?php
-
 require_once('./php/component.php')
-    ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carrito de compra</title>
-    <!-- BOOSTRAP  CDN-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css"
         integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
-    <!-- FONT ASWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
-
-    <!-- ESTILOS -->
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
@@ -26,15 +20,12 @@ require_once('./php/component.php')
         <div class="row text-center py-5">
             <?php
             component("avion" , 100, "./img/avion.png" , "Avion de juguete");
-            component("jug" , 50, "./img/jug.png" , "jug");
-            component("peluche" , 350, "./img/peluche.png", "Peluche bonito");
+            component("jug" , 50, "./img/jug.png" , "jugete para infantes");
+            component("peluche" , 350, "./img/peluche.png", "Peluche de pache de conejito");
             component("robot" , 4000, "./img/robot.png", "Robot asesino");
-
             ?>
-
         </div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
         crossorigin="anonymous"></script>
@@ -45,5 +36,4 @@ require_once('./php/component.php')
         integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa"
         crossorigin="anonymous"></script>
 </body>
-
 </html>
