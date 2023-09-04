f35491e24f33174ca2750e736fc05cc73e61fbbf
DanielBautistaMr <dbautista542@unab.edu.co>
2023-09-04 09:36:20 -0500

    Se corrigen errores del header y aparte se crea el sistema que guarda los productos

--- index.php
+++ index.php
@@ -1,9 +1,45 @@
 <?php
+
+session_start();
+
 require_once('./php/CreateDb.php');
 require_once('./php/component.php');
 
+
+
 // crear una instancia en la base de datos
 $database = new CreateDb("Productdb", "ProductosF");
+
+if (isset($_POST['add'])){
+    if(isset($_SESSION['cart'])){
+
+        $item_array_id = array_column($_SESSION['cart'], "product_id");
+
+        if(in_array($_POST['product_id'], $item_array_id)){
+            echo "<script>alert('EL PRODUCTO YA FUE AGREGADO')</script>";
+            echo "<script>window.location = 'index.php'</script>";
+        }else{
+
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'product_id' => $_POST['product_id']
+            );
+
+            $_SESSION['cart'][$count] = $item_array;
+            // print_r($_SESSION['cart']);
+        }
+
+    }else{
+
+        $item_array = array(
+                'product_id' => $_POST['product_id']
+        );
+
+        // Crear nueva variable de sesión
+        $_SESSION['cart'][0] = $item_array;
+        // print_r($_SESSION['cart']);
+    }
+}
 
 require_once('./php/component.php')
     ?>
